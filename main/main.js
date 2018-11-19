1d1b24a06e62842091f42168ca2ea8c7fc1b758b
wansk46 <44826049+wansk46@users.noreply.github.com>
2018-11-19 17:48:07 +0800

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -1,4 +1,21 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
-};
+module.exports = printReceipt;
+
+function printReceipt(array) {
+	var length = array.length;
+	for (var i=0; i<length; i++){
+		var obj = array[i];
+		var barcode = obj.barcode;
+		var name = obj.name;
+		var unit = obj.unit;
+		var price = obj.price;
+	}
+
+
+
+
+
+
+	onsole.log("***<没钱赚商店>购物清单***\n")
+}
+
+
