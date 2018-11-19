ff4aee83fd2414a1831b1b05be7d14dcd7610bb7
wansk46 <44826049+wansk46@users.noreply.github.com>
2018-11-20 01:47:17 +0800

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -1,21 +1,39 @@
-module.exports = printReceipt;
 
-function printReceipt(array) {
-	var length = array.length;
-	for (var i=0; i<length; i++){
-		var obj = array[i];
-		var barcode = obj.barcode;
-		var name = obj.name;
-		var unit = obj.unit;
-		var price = obj.price;
+
+module.exports = function printInventory (inputs){
+	let result = '***<没钱赚商店>购物清单***\n';
+	let count = 0;
+	let total = 0;
+	let temp = [];
+	let lastItem;
+
+	for (let i=0; i<inputs.length; i++)
+	{
+		if(!temp.includes(inputs[i].barcode))
+		{
+			temp.push(inputs[i].barcode);
+		}
 	}
 
+	for (let i=0; i<temp.length; i++)
+	{
+		for (let j=0; j<inputs.length; j++)
+		{
+			if(temp[i]===(inputs[j].barcode)){
+				count++;
+				lastItem = j;
+
+			}
+		}
+
+		result += '名称：'+inputs[lastItem].name+'，数量：'+count+inputs[lastItem].unit+'，单价：'+inputs[lastItem].price.toFixed(2)+'(元)，小计：'+(count*inputs[lastItem].price).toFixed(2)+'(元)\n';
+		total += count * inputs[lastItem].price;
+		count=0;
+	}
+	result +='----------------------\n' + '总计：'+total.toFixed(2)+'(元)\n' + '**********************';
+	return result;
 
 
 
-
-
-	onsole.log("***<没钱赚商店>购物清单***\n")
 }
-
 
